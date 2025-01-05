f0e43b212c29fd4dcde387162cf072cf5573966b
SimoneVerzeletti <simone.verzeletti@aifos.it>
2025-01-05 17:57:54 +0100

    feat: endWork

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let num = 1;
+
+for ( num >= 1; num <= 100; num++){
+    if (num % 3===0 && num %5===0){
+        console.log(`FizzBuzz`); // Stampa FizzBuzz se è multiplo di 3 e di 5
+    }
+    else if (num % 3===0 ){
+        console.log(`Fizz`); // Stampa il numero solo se non è multiplo di 3
+    } else if (num % 5===0 ){
+        console.log(`Buzz`); // Stampa il numero solo se non è multiplo di 5
+    } else {
+        console.log(num); // Stampa il numero solo se non è multiplo di 3 o 5
+    }
+    }
+
