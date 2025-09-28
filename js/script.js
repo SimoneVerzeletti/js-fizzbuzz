b6e56fd54580779b497e7e2bd16e7119031837fc
SimoneVerzeletti <simone.verzeletti@aifos.it>
2025-09-28 12:14:36 +0200

    edit: sistemazione commenti

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ let x = 0;
 //Creare il ciclo per stampare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
   x = i;
+  //Scope per controllare se sono multipli di 3 e 5
   if (x % 3 === 0 && x % 5 === 0) {
     console.log(x + " FizzBuzz");
+    //Scope per controllare se sono multipli di 3 o 5
   } else if (x % 3 === 0) {
     console.log(x + " Fizz");
   } else if (x % 5 === 0) {
@@ -14,9 +16,3 @@ for (let i = 1; i <= 100; i++) {
     console.log(x);
   }
 }
-
-//Scope per controllare se sono multipli di 3 o 5
-
-//Scope per controllare se sono multipli di 3 e 5
-
-//Stampa risultato
